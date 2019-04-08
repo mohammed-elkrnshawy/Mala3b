59a4ca24a02cc6d99e7842f666c86be222779863
Mahmoud Hlal <mahmoudhlal741996@gmail.com>
2019-04-08 23:26:00 +0200

    'register'

---
+++ app/src/main/java/com/zt/mala3b/Activity/SignUp/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.zt.mala3b.Activity.SignUp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zt.mala3b.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
