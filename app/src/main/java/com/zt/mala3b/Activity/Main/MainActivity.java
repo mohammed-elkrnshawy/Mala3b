9326ee8fad851344790e62277aa9f9d281aca754
mohammedel_elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-04-17 23:16:26 +0200

    Start Bind

--- app/src/main/java/com/zt/mala3b/Activity/Main/MainActivity.java
+++ app/src/main/java/com/zt/mala3b/Activity/Main/MainActivity.java
@@ -1,15 +1,42 @@
 package com.zt.mala3b.Activity.Main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.zt.mala3b.Activity.Login.LoginActivity;
+import com.zt.mala3b.Activity.SignUp.SignUpActivity;
 import com.zt.mala3b.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnRegister)
+    Button btnRegister;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnLogin,R.id.btnRegister}) void Clicked(View view){
+        switch (view.getId()){
+            case R.id.btnLogin:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+
+            case R.id.btnRegister:
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/zt/mala3b/Activity/Splash/SplashActivity.java
+++ app/src/main/java/com/zt/mala3b/Activity/Splash/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.zt.mala3b.Activity.Splash;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zt.mala3b.Activity.SelectRole.SelectRoleActivity;
 import com.zt.mala3b.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +14,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                Intent intent=new Intent(SplashActivity.this, SelectRoleActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+
+
     }
 }
